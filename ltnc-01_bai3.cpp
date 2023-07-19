b236910125f882db508f8e1cfa870ba52db1db42
Nguyen Tuan Anh <tuananh01677606@gmail.com>
2023-07-19 11:09:58 +0700

    bai3

---
+++ ltnc-01_bai3.cpp
@@ -0,0 +1,21 @@
+// #include <iostream>
+// #include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl
+         << b << endl
+         << c << endl
+         << fixed << setprecision(3) << d << endl
+         << fixed << setprecision(9) << e << endl; 
+    return 0;
+}
+
