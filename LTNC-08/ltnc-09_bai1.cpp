19fc039b37bd5144bcfa6ee51fb0dd0610b03ffe
tuanah04 <124872748+tuanah04@users.noreply.github.com>
2023-08-09 09:22:29 +0700

    ltnc09_bai1

---
+++ LTNC-08/ltnc-09_bai1.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    string s;
+    getline(cin, s);
+    cout << "Hello, World." << endl;
+    cout << s << endl;
+    return 0;
+}
+
